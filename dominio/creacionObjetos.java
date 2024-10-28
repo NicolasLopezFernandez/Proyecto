c21d052120971a3c850ce4373a9ae94e711538aa
Nicolás <nicolas.lopezfernandez1@usp.ceu.es>
2024-10-28 11:09:37 +0100

    creacionObjetos.java

---
+++ dominio/creacionObjetos.java
@@ -0,0 +1,11 @@
+pubic class portatiles {
+  private String CPU
+  private String GPU
+  private String RAM
+  private String Marca
+
+  public portatil(String CPU, String GPU, String RAM, String Marca);
+    this.CPU = CPU;
+    this.GPU = GPU;
+    this.RAM = RAM;
+    this.Marca = Marca;
