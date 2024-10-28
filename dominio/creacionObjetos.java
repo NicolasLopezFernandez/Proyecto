af4ade550a7a0815346fc5009b53dac0a5f16507
Nicolás <nicolas.lopezfernandez1@usp.ceu.es>
2024-10-28 11:52:23 +0100

    Update creacionObjetos.java

--- dominio/creacionObjetos.java
+++ dominio/creacionObjetos.java
@@ -1,11 +1,17 @@
 pubic class portatiles {
   private String CPU
   private String GPU
-  private String RAM
+  private int RAM
   private String Marca
 
-  public portatil(String CPU, String GPU, String RAM, String Marca);
+  public portatil(String CPU, String GPU, int RAM, String Marca) {;
     this.CPU = CPU;
     this.GPU = GPU;
     this.RAM = RAM;
     this.Marca = Marca;
+  }
+  public String detallesDelPortatil toString() {
+    StringBuilder sb = new String StringBuilder();
+    sb.append "CPU: " .append CPU .append " GPU: " .append GPU .append " RAM: " .append RAM .append " Marca: " .append Marca;
+  }
+}
