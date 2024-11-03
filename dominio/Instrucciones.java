13b5a8c1f300dbcbe59ce7d1bddf2080093e191e
Nicolas Lopez Fernandez <nicolas.lopezfernandez1@usp.ceu.es>
2024-11-03 01:56:28 +0100

    Trabajo Sabado

--- dominio/Instrucciones.java
+++ dominio/Instrucciones.java
@@ -2,42 +2,31 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SistemaLocalidades implements Serializable {
-    private List<Provincia> provincias = new ArrayList<>();
+public class TiendaElectronica implements Serializable {
+    private List<Productos> Caracteristicas = new ArrayList<>();
 
-    public void addProvincia(Provincia provincia) {
-        provincias.add(provincia);
+    public void addcpu(Cpu cpu) {
+        cpu.add(cpu);
+        return;
     }
 
-    public void addMunicipio(String nombreProvincia, Municipio municipio) {
-        for (Provincia prov : provincias) {
-            if (prov.nombre.equals(nombreProvincia)) {
-                prov.addMunicipio(municipio);
-                return;
-            }
-        }
+    public void addGpu(Gpu gpu) {
+        addGPU(gpu);
+        return;
     }
 
-    public void addLocalidad(String nombreProvincia, String nombreMunicipio, Localidad localidad) {
-        for (Provincia prov : provincias) {
-            if (prov.nombre.equals(nombreProvincia)) {
-                for (Municipio muni : prov.municipios) {
-                    if (muni.nombre.equals(nombreMunicipio)) {
-                        muni.addLocalidad(localidad);
-                        return;
-                    }
-                }
-            }
-        }
+    public void addRAM(RAM ram) {
+        addRAM(ram);
+        return;
     }
 
     public void listData() {
-        provincias.forEach(System.out::println);
+        Caracteristicas.forEach(System.out::println);
     }
 
     public void saveData(String filename) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
-            out.writeObject(provincias);
+            out.writeObject(Caracteristicas);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -45,7 +34,7 @@ public class SistemaLocalidades implements Serializable {
 
     public void loadData(String filename) {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
-            provincias = (List<Provincia>) in.readObject();
+            Caracteristicas = (List<Caracteristicas>) in.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
